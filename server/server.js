393ceb120ae825747600488ddb1d0ff5c2914e76
Martin Pfeiffer <pfeiffer.martin@hotmail.com>
2021-12-29 18:34:06 +0100

    message

--- server/server.js
+++ server/server.js
@@ -14,16 +14,10 @@ app.set('views', path.join(__dirname, '..', 'views'));
 app.set('view engine', 'ejs');
 
 
-
-
-
 app.get("/",function(req, res){
 
-  const url = "https://api.openweathermap.org/data/2.5/weather?q=Salzburg&appid=d57d2c7994d3ddeb7681ec6ba847752e";
 
-
-
-  var root = path.join(__dirname, '..', 'index.html');
+  
   res.render("index");
 
 });
