577a4580ffceddd64b76740eeef8d9b3772830f7
Martin Pfeiffer <pfeiffer.martin@hotmail.com>
2021-12-29 18:34:40 +0100

    changed directories

--- server.js
+++ server.js
@@ -1,26 +1,26 @@
 const express = require("express");
 const https = require("https");
 const path = require("path");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
 
 const port = 3000;
-
+const linkToCss = path.join(__dirname, "..", "css", "styles.css");
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.set('views', path.join(__dirname, '..', 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "..", "views"));
+app.set("view engine", "ejs");
 
 
 app.get("/",function(req, res){
-
-
-  
-  res.render("index");
+  console.log(linkToCss);
+  res.render("index", {linkToCss: linkToCss});
 
 });
+
+
 
 app.listen(port, function(){
   console.log("Server runs on Port : " + port);
