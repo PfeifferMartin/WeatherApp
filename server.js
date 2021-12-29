4ac94157c57869f5415c5aedf35dc6cae73784d4
Martin Pfeiffer <pfeiffer.martin@hotmail.com>
2021-12-29 19:01:56 +0100

    ficed the stylesheet

--- server.js
+++ server.js
@@ -5,18 +5,18 @@ const bodyParser = require("body-parser");
 
 
 const port = 3000;
-const linkToCss = path.join(__dirname, "..", "css", "styles.css");
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
-app.set("views", path.join(__dirname, "..", "views"));
+
+app.set("views", path.join(__dirname,"views"));
 app.set("view engine", "ejs");
 
 
 app.get("/",function(req, res){
-  console.log(linkToCss);
-  res.render("index", {linkToCss: linkToCss});
+  res.render("index");
 
 });
 
